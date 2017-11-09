0f13c786c5768297477f96f7ea4e4ffc5f1b15d6
Nicholas Sugimoto <nlsugi10@gmail.com>
2017-11-09 12:06:58 -0800

    Implement bootstrap themed dropdown.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // https://www.w3schools.com/jsref/met_doc_write.asp
-document.write("<p>This statement writes into the document a paragraph.</p>");
-document.write("<h1>Note that this stuff is written before the body since it's in the head.</h1>")
+// document.write("<p>This statement writes into the document a paragraph.</p>");
+// document.write("<h1>Note that this stuff is written before the body since it's in the head.</h1>")
 
-console.log("This will be seen in your browsers console (right click > inspect element > console)")
+// console.log("This will be seen in your browsers console (right click > inspect element > console)")
